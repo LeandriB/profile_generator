ecf8904ab527a8188193d399cdd88585c70ce302
Leandri Beavis <leandrikuyk@gmail.com>
2022-06-13 00:53:05 -0400

    Refactor and seperated manager questions due to TypeError

--- index.js
+++ index.js
@@ -5,11 +5,45 @@ const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
+// Import function to generate HTML
+const generateHtml = require('./src/generateHtml')
 
 const teamMembers = [];
 
+const addManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the managers name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the managers id?', 
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the managers email?', 
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the managers office number?',
+        },
+    ])
+    .then(data => {
+        const manager = new Manager(data.name, 'Manager', data.id, data.email, data.officeNumber);
+        console.log(manager);
+        teamMembers.push(manager);
+    })
+    .then(addEmployee)
+};
+
 // Array of questions for user input
-const addEmployee = [
+const addEmployee = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -20,7 +54,6 @@ const addEmployee = [
             name: 'role',
             message: 'Select the team members role:',
             choices: [
-                'Manager',
                 'Engineer', 
                 'Intern'
             ]
@@ -37,122 +70,71 @@ const addEmployee = [
         },
         {
             type: 'input',
-            name: 'officeNumber',
-            message: 'What is the managers office number?', 
-            // TODO add when: statement
-            when: (input) => input.role === 'Manager',
-        },
-        {
-            type: 'input',
             name: 'github',
             message: 'What is the engineers github username?', 
-            // TODO add when: statement
             when: (input) => input.role === 'Engineer',
         },
         {
             type: 'input',
             name: 'school',
             message: 'What is the interns school name?', 
-            // TODO add when: statement
             when: (input) => input.role === 'Intern',
         },
         {
             type: 'confirm',
             name: 'addMore',
-            message: 'Would you like to an additional team members?',
+            message: 'Would you like to an additional team member?',
             default: false
         }
+    ]).then(teamMemberData => {
+            let { role, name, id, email, github, school, addMore} = teamMemberData;
+            let employee;
 
-    ]
-    // .then(employeeInfo => {
-        
-    //     let {role, name, id, email, github, school, officeNumber, addMore} = employeeInfo;
-    //     let employee;
+            if (role === 'Engineer') {
+                employee = new Engineer(name, id, email, github);
+                console.log(employee);
+                teamMembers.push(employee);
 
-    //     if(role === 'Manager'){
-    //         employee = new Manager(name, id, email, officeNumber);
-    //         console.log(employee);
-    //     } else if(role === 'Engineer') {
-    //         employee = new Engineer(name, id, email, github);
-    //         console.log(employee);
-    //     } else if(role === 'Intern') {
-    //         employee = new Intern(name, id, email, school);
-    //         console.log(employee)
-    //     } else {
-    //         return employee// Refactor switch statement??
-    //     };
-
-    //     teamMembers.push(employee)
-    //     // allows user to add additional team members
-    //     if(addMore){
-    //         return addEmployee(teamMembers);
-    //     } else {
-    //         return teamMembers;
-    //     }
-    // });
-
-// addEmployee();
-
-
-// TODO: Write function to writefile
-const writeToFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(`./dist/index.html`, fileContent, err => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve({
-                ok: true,
-                message: 'Team profile was generated'
-            });
-        });
-    });
-};
-
-// TODO: Write function to initialize application
-function init() {
-    return inquirer.prompt(addEmployee)
-    .then(employeeInfo => {
-        
-        let {role, name, id, email, github, school, officeNumber, addMore} = employeeInfo;
-        let employee;
-
-        if(role === 'Manager'){
-            employee = new Manager(name, id, email, officeNumber);
-            console.log(employee);
-        } else if(role === 'Engineer') {
-            employee = new Engineer(name, id, email, github);
-            console.log(employee);
-        } else if(role === 'Intern') {
-            employee = new Intern(name, id, email, school);
-            console.log(employee)
-        } else {
-            return employee// Refactor switch statement??
-        };
-
-        teamMembers.push(employee)
-        // allows user to add additional team members
-        if(addMore){
-            return addEmployee(teamMembers);
-        } else {
-            return teamMembers;
-        }
+            } else if (role === 'Intern') {
+                employee = new Intern(name, id, email, school);
+                console.log(employee)
+                teamMembers.push(employee);
+        } 
     });
 }
 
-// Function call to initialize app
+
+// // TODO: Write function to writefile
+// const writeToFile = data => {
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile(`./dist/index.html`, generateHtml(teamMembers), err => {
+//             if (err) {
+//                 reject(err);
+//                 return;
+//             }
+//             resolve({
+//                 ok: true,
+//                 message: 'Team profile was generated'
+//             });
+//         });
+//     });
+// };
+
+// TODO: Write function to initialize application
+function init() {
+    addManager()
+}
+
+// // Function call to initialize app
+// init()
+//     .then(pageHtml => {
+//         return writeToFile(pageHtml);
+//     })
+//     .then(writeFileResponse => {
+//         console.log(writeFileResponse);
+//     })
+//         .catch(err => {
+//         console.log(err);
+//     });;
+
 init()
-    .then(teamMembers => {
-        console.log(teamMembers);
-        return generateHtml(teamMembers);
-    })
-    .then(pageHtml => {
-        return writeToFile(pageHtml);
-    })
-    .then(writeFileResponse => {
-        console.log(writeFileResponse);
-    })
-        .catch(err => {
-        console.log(err);
-    });;
